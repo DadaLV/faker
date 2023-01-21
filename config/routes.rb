e2d112fc59bc2978f0d90982d5443722d7e2d2e5
DadaLV <vynohradova.l.a@gmail.com>
2023-01-21 03:14:55 +0200

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'questions/index'
   get 'questions/some_action'
+  get 'questions/the_action/:some_param', to: 'questions#some_action', prm: 'the_prm'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
